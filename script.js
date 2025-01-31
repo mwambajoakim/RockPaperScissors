f6b8a657915de6e8ab4643beeee749515d94508c
mwamba <mwambajoakim@gmail.com>
2025-01-31 16:34:14 +0300

    Create computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Hello Joakim!");
+function getComputerChoice() { 
+
+let rand = Math.floor(Math.random() * 10);
+
+if (rand === 0 && rand >= 4) {
+    return;
+}
+
+else {
+    if (rand === 1) {
+        return "Rock";
+    }
+
+    else if (rand === 2) {
+        return "Paper";
+    }
+
+    else if (rand === 3) {
+        return "Scissors";
+    }
+}
+}
+
+console.log(getComputerChoice());
+
