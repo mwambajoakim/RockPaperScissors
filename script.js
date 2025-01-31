c704b4f17ea87f2fe1f0a4f17a7fb7e058b5f762
mwamba <mwambajoakim@gmail.com>
2025-01-31 16:41:07 +0300

    Update comments

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
+//Create function for computer choice
 function getComputerChoice() { 
 
+//Get random numbers
 let rand = Math.floor(Math.random() * 10);
 
+//Exclusively get three numbers; 1, 2, and 3
 if (rand === 0 && rand >= 4) {
     return;
 }
 
 else {
+    //Assign "Rock" to 1
     if (rand === 1) {
         return "Rock";
     }
-
+    //Assign "Paper" to 2
     else if (rand === 2) {
         return "Paper";
     }
-
+    //Assign "Scissors" to 3
     else if (rand === 3) {
         return "Scissors";
     }
